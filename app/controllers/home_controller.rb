7bd1895966aecabbf050d3c0378800cd75c87d47
Andrew Carmer <carmerandrew@gmail.com>
2015-10-07 16:16:35 -0600

    add customer and landing page spec

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+
+  def front_page
+  end
+end
---
+++ spec/features/landing_page_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.feature "Landing page", type: :feature do
+  scenario "User visits homepage" do
+
+    visit "/"
+
+    expect(page).to have_content("House of Luthiery")
+    expect(page).to have_css("#banner")
+  end
+end
