feaa37625a3b532ad5aa6c33e4dd6ce1ac9140ce
Andrew Carmer <carmerandrew@gmail.com>
2015-12-18 14:27:22 -0700

    add instrument controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: "home#home"
 
-  get "/about",            to: "home#about",            as: :about
-  get "/research",         to: "home#research",         as: :research
-  get "/custom-shop",      to: "home#custom_shop",      as: :custom_shop
-  get "/signature-models", to: "home#signature_models", as: :signature_models
-  get "/home",             to: "home#home",             as: :home
+  get "/about",            to: "home#about",                   as: :about
+  get "/research",         to: "home#research",                as: :research
+  get "/custom-shop",      to: "instruments#custom_shop",      as: :custom_shop
+  get "/signature-models", to: "instruments#signature_models", as: :signature_models
+  get "/home",             to: "home#home",                    as: :home
 end
