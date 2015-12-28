f84cf3ea82e02a1a53a62eec76ed63b20185a11a
Andrew Carmer <carmerandrew@gmail.com>
2015-12-28 11:42:50 -0700

    new content

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,7 +1,7 @@
 class Instrument < ActiveRecord::Base
   has_attached_file :image, styles: { large: "300X300",
                                       medium: "200X200>",
-                                      thumb: "100x100>" }, default_url: "/missing.png"
+                                      thumb: "100x100>" }, default_url: "product_photo_coming_soon.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates :name, presence: true, uniqueness: true
   validates :slug, presence: true, uniqueness: true
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ActiveRecord::Base
   has_attached_file :image, styles: { large: "600X600",
                                       medium: "300x300>",
-                                      thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+                                      thumb: "100x100>" }, default_url: "product_photo_coming_soon.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   belongs_to :instrument
